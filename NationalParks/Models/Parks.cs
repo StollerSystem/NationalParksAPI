ef4e6cf4505fa589116fef120460db33d59a90f7
Ben Stoller <stollersystemsound@gmail.com>
2020-10-30 08:37:26 -0700

    set up initial models

---
+++ NationalParks/Models/Parks.cs
@@ -0,0 +1,9 @@
+namespace NationalParks.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string ParkName { get; set; }
+    public string ParkLocation { get; set; }
+  }
+}
