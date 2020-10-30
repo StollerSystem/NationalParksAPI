e68930d6524a2a3badc3ecc0d3d1812a23c262f0
Ben Stoller <stollersystemsound@gmail.com>
2020-10-30 09:46:50 -0700

    start setting up JWT

---
+++ NationalParks/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace NationalParks.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
--- NationalParks/Program.cs
+++ NationalParks/Program.cs
@@ -20,5 +20,6 @@ namespace NationalParks
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                // .UseUrls("http://localhost:4000");
     }
 }
